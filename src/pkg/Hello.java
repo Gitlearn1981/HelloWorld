f212871ab8a5d6479ebcf93a86ec268c030a08a2
Akinari Masuyama <p5555@gmail.com>
2020-11-09 02:16:35 +0900

    コメント除去

--- src/pkg/Hello.java
+++ src/pkg/Hello.java
@@ -3,7 +3,6 @@ package pkg;
 public class Hello {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("Hello World!");
 		System.out.println("masuyama1107");
 	}
